2d50d38ff46a00691054f383b27847697c0d048f
Tarunjeev <59491659+Tarunjeev@users.noreply.github.com>
2020-11-01 09:58:13 -0800

    Add files via upload

---
+++ histogram_web_app.R
@@ -0,0 +1,51 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+data("airquality")
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Ozone level!"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 40,
+                        value = 20)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- airquality$Ozone
+        x    <- na.omit(x)
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = '#75AADB', border = 'black', xlab = "Ozone level", main = "Histogram of Ozone level")
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
